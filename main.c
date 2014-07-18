5bbe97f6a7bf38ac87f07d8c8073ac744940c181
changhan <changhanlee@gmail.com>
2014-07-18 16:31:53 -0400

    2

--- main.c
+++ main.c
@@ -4,14 +4,51 @@
 
 #define ELEMENTS 16
 
+void initbox(int* array){
+  static int counter;
+  counter = 0;
+  while(counter<ELEMENTS){
+    *(array+counter) = 0;
+    ++counter;
+  }
+}
+
+void printbox(int* array){
+  static int counter;
+  counter = 0;
+  while(counter<ELEMENTS){
+    printf("%d",*(array+counter));
+    ++counter;
+    if(counter % 4 == 0){
+      printf("\n");
+    }
+  }
+}
+
+
 int main(){
 
   int* box = malloc (sizeof(int) * ELEMENTS);
+  initbox(box);
+
+
+  *(box+2) = 2;
+  *(box+4) = 6;
+
+  printbox(box);
 
   srand(time(NULL));
   int r = rand() % 16;
-  printf("%d\n",r);
+
+
+
+
+
 
   free(box);
   return 0;
+
+
+
+
 }
